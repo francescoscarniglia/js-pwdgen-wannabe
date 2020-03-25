e58f125d3a9e3a21712bc0f3069602b795a8b132
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-03-25 16:03:38 +0100

    Exercise ok

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-/* 1. RICEVO IL NOME DALL'UTENTE */
+/* 1. NAME FROM THE USER */
 var whatsName = prompt('what is your name?');
 console.log(whatsName);
 
-/* 2. RICEVO IL COGNOME DALL'UTENTE */
+/* 2.SURNAME FROM THE USER */
 var whatsSurname = prompt('what is your surname?');
 console.log(whatsSurname);
 
-/* 3. RICEVO IL COLORE PREFERITO DALL'UTENTE */
+/* 3.FAVOURITE COLOR FROM THE USER */
 var whatsColor = prompt('what is your favourite color?')
 console.log(whatsColor);
 
-/* 4. RIPORTO IN HTML IL RISULTATO */
-// document.getElementById('name').innerHTML = whatsName;
+/* 4. PRINT RESULTS IN HTML */
 document.getElementById('name').innerHTML = whatsName + whatsSurname + whatsColor + 20;
